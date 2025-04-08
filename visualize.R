14c588af69bc03dc27d6856e920888b6a62dd182
ch-unine-057489 <ch-unine-057489@noto.epfl.ch>
2025-04-08 11:54:37 +0200

    visualizer

---
+++ visualize.R
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .R
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+x = c(1:100)
+
